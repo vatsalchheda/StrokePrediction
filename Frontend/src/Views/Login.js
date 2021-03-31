584efc6066518092672ad4743bebe542193f0677
vatsalchheda <vatsalchheda007@gmail.com>
2021-03-31 14:53:38 +0530

    Background Image Styling

--- Frontend/src/Views/Login.js
+++ Frontend/src/Views/Login.js
@@ -1,20 +1,30 @@
-import React,{useEffect} from "react"
-import TextField from "@material-ui/core/TextField"
-import Button from "@material-ui/core/Button"
-import "./Login.css"
-import img from "./Stroke.png"
+import React, { useEffect } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import './Login.css';
+import img from './Stroke.png';
 
-function Login(){
-
-    const [username,setUsername] = React.useState(null);
-    const [password,setPassword] = React.useState.apply(null)
-    return(
-        <div className="center">
-            <div  className="main">
-                <h1>bhavya</h1>
-            </div>
-        </div>
-    )
+function Login() {
+  const [username, setUsername] = React.useState(null);
+  const [password, setPassword] = React.useState(null);
+  return (
+    <div className="container">
+      <div className="Login">
+        <TextField
+          label="Username"
+          id="standard-basic"
+          onChange={(e) => setUsername(String(e.target.value))}
+          value={username}
+        />
+        <TextField
+          label="Password"
+          id="standard-basic"
+          onChange={(e) => setPassword(String(e.target.value))}
+          value={password}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
